f11728d5fc5cd8d007ec8572e2ffcf7ccf5caba1
romerorafael <60353241+romerorafael@users.noreply.github.com>
2022-09-05 17:34:34 -0300

    Atualizando o CSS e o HTML

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,6 +1,7 @@
 const headerElement = document.getElementById('header'); // elemento alvo
 const pixelsAmount = '100'; // Quantidade de pixels a contar do TOP até definir a cor
 
+//Função que fica observando o scroll do usuário para alteração da cor do header
 window.addEventListener('scroll', function() {
     if (window.scrollY > pixelsAmount) {
         headerElement.classList.add('change-style'); // adiciona classe "changeColor"
